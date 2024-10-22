ab2e7f0165f68d2a3471af7b53899ed6c7b5bb41
Haider Mahmood <haider.slsc@gmail.com>
2024-10-23 03:35:27 +0500

    Completed signup flow * Store user in local storage * Maked private component * Handle signup page with local storage * Update navbar with logout and signup menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
+import PrivateComponent from './components/PrivateComponent';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
@@ -10,12 +11,15 @@ const App = () => {
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path = '/' element = { <h1>Products Listing Component</h1> } />
-          <Route path = '/add' element = { <h1>Add Product Component</h1> } />
-          <Route path = '/update' element = { <h1>Update Product Component</h1> } />
-          <Route path = '/profile' element = { <h1>Profile Component</h1> } />
-          <Route path = '/logout' element = { <h1>Logout Component</h1> } />
-          <Route path = '/signup' element = { <SignUp /> } />
+          <Route element={<PrivateComponent />}>
+            <Route path='/' element={<h1>Products Listing Component</h1>} />
+            <Route path='/add' element={<h1>Add Product Component</h1>} />
+            <Route path='/update' element={<h1>Update Product Component</h1>} />
+            <Route path='/profile' element={<h1>Profile Component</h1>} />
+            <Route path='/logout' element={<h1>Logout Component</h1>} />
+          </Route>
+
+          <Route path='/signup' element={<SignUp />} />
         </Routes>
         <Footer />
       </BrowserRouter>
